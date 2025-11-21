563b1c2a3d1fe534639070af15011756e0b2bc56
usmanBoi <usmankith@gmail.com>
2025-11-21 23:51:09 +0500

    +reviews getOne baseProcedure ,+reviews createProtectedProcedure, added in trpc/routers

---
+++ src/modules/reviews/server/procedures.ts
@@ -0,0 +1,104 @@
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { TRPCError } from "@trpc/server";
+import z from "zod";
+
+export const reviewsRouter = createTRPCRouter({
+  getOne: protectedProcedure
+    .input(
+      z.object({
+        productId: z.string(),
+      }),
+    )
+    // destructuring ctx and input
+    .mutation(async ({ ctx, input }) => {
+      const product = await ctx.payload.findByID({
+        collection: "products",
+        id: input.productId,
+      });
+
+      if (!product) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Product not found",
+        });
+      }
+
+      const reviewsData = await ctx.payload.find({
+        collection: "reviews",
+        limit: 1,
+        where: {
+          and: [
+            {
+              product: {
+                equals: product.id,
+              },
+            },
+            {
+              user: {
+                equals: ctx.session.user.id,
+              },
+            },
+          ],
+        },
+      });
+
+      const review = reviewsData.docs[0];
+      if (!review) return null;
+      return review;
+    }),
+  create: protectedProcedure
+    .input(
+      z.object({
+        productId: z.string(),
+        rating: z.number().min(1, { message: "Rating is required" }).max(5),
+        description: z.string().min(2, { message: "Description is required" }),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const product = await ctx.payload.findByID({
+        collection: "products",
+        id: input.productId,
+      });
+
+      if (!product) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Product not found",
+        });
+      }
+      const existingReviewsData = await ctx.payload.find({
+        collection: "reviews",
+        // limit: 1,
+        where: {
+          and: [
+            {
+              product: {
+                equals: input.productId,
+              },
+            },
+            {
+              user: {
+                equals: ctx.session.user.id,
+              },
+            },
+          ],
+        },
+      });
+      if (existingReviewsData.totalDocs > 0) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "You have already reviewed this product",
+        });
+      }
+      const review = await ctx.payload.create({
+        collection: "reviews",
+        data: {
+          user: ctx.session.user.id,
+          product: product.id,
+          rating: input.rating,
+          description: input.description,
+        },
+      });
+      return review;
+    }),
+});
